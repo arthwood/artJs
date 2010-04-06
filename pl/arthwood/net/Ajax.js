9d13583a6d2fc821442d2d2c47f3bedf55674a16
arthwood <arthwood@llp.pl>
2010-04-07 00:19:18 +0200

    Ajax improvements

--- pl/arthwood/net/Ajax.js
+++ pl/arthwood/net/Ajax.js
@@ -11,16 +11,19 @@ ArtJs.Ajax = pl.arthwood.net.Ajax = function(url, data, method) {
   
   this.url = url;
   this.data = data;
-  this.method = method || methods.GET;
+  this.method = method;
+  this.requestData = data;
+  this.requestMethod = method || methods.GET;
   
-  if (ArtJs.ArrayUtils.include([methods.PUT, methods.DELETE], this.method)) {
-    method = methods.POST;
-    this.data._method = this.method;
+  if (!ArtJs.ArrayUtils.include(ArtJs.Ajax.SupportedMethods, this.requestMethod)) {
+    this.requestData = this.requestData || {};
+    this.requestData._method = this.requestMethod;
+    this.requestMethod = methods.POST;
   }
   
   var r = this._request = new XMLHttpRequest();
   
-  r.open(method, this.url, true);  
+  r.open(this.requestMethod, this.url, true);  
   
   this.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   this.setRequestHeader('X-ArtJs-Version', ArtJs.VERSION);
@@ -32,72 +35,73 @@ ArtJs.Ajax = pl.arthwood.net.Ajax = function(url, data, method) {
   r.onerror = this.onErrorDelegate;
 };
 
-ArtJs.Ajax.prototype.request = function() {
-  var data;
-  
-  if (this.method == ArtJs.Ajax.Methods.POST) {
-    this.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+ArtJs.Ajax.prototype = {
+  request: function() {
+    var data;
     
-    if (this.data) data = ArtJs.ObjectUtils.toQueryString(this.data);
+    if (this.requestMethod == ArtJs.Ajax.Methods.POST) {
+      this.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+      
+      if (this.requestData) data = ArtJs.ObjectUtils.toQueryString(this.requestData);
+    }
+    
+    this._request.send(data);
+  },
+
+  abort: function() {
+    this._request.abort();
+  },
+
+  onReadyStateChange: function(event) {
+  },
+
+  onProgress: function(event) {
+    var r = event.position / event.totalSize;
+    
+    this.onProgress.fire(this, r);
+  },
+
+  onLoad: function(event) {
+    this.onSuccess.fire(this);
+  },
+
+  onError: function(event) {
+    p('onError');
+    p(event);
+  },
+
+  getReadyState: function() {
+    return this._request.readyState;
+  },
+
+  getStatus: function() {
+    return this._request.status;
+  },
+
+  getStatusText: function() {
+    return this._request.statusText;
+  },
+
+  getResponseText: function() {
+    return this._request.responseText;
+  },
+  
+  getResponseXML: function() {
+    return this._request.responseXML;
+  },
+
+  setRequestHeader: function(header, value) {
+    return this._request.setRequestHeader(header, value);
+  },
+
+  getResponseHeader: function(header) {
+    return this._request.getResponseHeader(header);
+  },
+  
+  getAllResponseHeaders: function() {
+    return this._request.getAllResponseHeaders();
   }
-  
-  this._request.send(data);
 };
-
-ArtJs.Ajax.prototype.abort = function() {
-  this._request.abort();
-};
-
-ArtJs.Ajax.prototype.onReadyStateChange = function(event) {
-};
-
-ArtJs.Ajax.prototype.onProgress = function(event) {
-  var r = event.position / event.totalSize;
-  
-  this.onProgress.fire(this, r);
-};
-
-ArtJs.Ajax.prototype.onLoad = function(event) {
-  this.onSuccess.fire(this);
-};
-
-ArtJs.Ajax.prototype.onError = function(event) {
-  p('onError');
-  p(event);
-};
-
-ArtJs.Ajax.prototype.getReadyState = function() {
-  return this._request.readyState;
-};
-
-ArtJs.Ajax.prototype.getStatus = function() {
-  return this._request.status;
-};
-
-ArtJs.Ajax.prototype.getStatusText = function() {
-  return this._request.statusText;
-};
-
-ArtJs.Ajax.prototype.getResponseText = function() {
-  return this._request.responseText;
-};
-
-ArtJs.Ajax.prototype.getResponseXML = function() {
-  return this._request.responseXML;
-};
-
-ArtJs.Ajax.prototype.setRequestHeader = function(header, value) {
-  return this._request.setRequestHeader(header, value);
-};
-
-ArtJs.Ajax.prototype.getResponseHeader = function(header) {
-  return this._request.getResponseHeader(header);
-};
-
-ArtJs.Ajax.prototype.getAllResponseHeaders = function() {
-  return this._request.getAllResponseHeaders();
-};
-
 
 ArtJs.Ajax.Methods = {
   GET: 'GET',
@@ -105,7 +109,7 @@ ArtJs.Ajax.Methods = {
   PUT: 'PUT',
   DELETE: 'DELETE'
 };
-
+ArtJs.Ajax.SupportedMethods = [ArtJs.Ajax.Methods.GET, ArtJs.Ajax.Methods.POST];
 ArtJs.Ajax.ReadyState = {
   UNINITIALIZED: 0,
   OPEN: 1,
