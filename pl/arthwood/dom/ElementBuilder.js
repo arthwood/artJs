f3e47b74de0a1ca3dccd6f9f2cc033dd66f2cde8
artur bilski <arthwood@llp.pl>
2010-04-14 13:32:08 +0200

    fixed error in ElementBuilder.prototype.toString

--- pl/arthwood/dom/ElementBuilder.js
+++ pl/arthwood/dom/ElementBuilder.js
@@ -5,29 +5,35 @@ ArtJs.ElementBuilder = pl.arthwood.dom.ElementBuilder = function(name, attribute
   this.empty = Boolean(empty);
 };
 
-ArtJs.ElementBuilder.prototype.toString = function() {
-  var attributes = ArtJs.ObjectUtils.isEmpty(this.attributes) ? '' : (' ' + this.attributesString() + ' ');
-  var part;
-
-  if (this.value) {
-    part = '>' + this.value + '</' + this.name + '>';
-  }
-  else {
-    if (this.empty) {
-      part = '/>';
+ArtJs.ElementBuilder.prototype = {
+  toString: function() {
+    var attributes = ArtJs.ObjectUtils.empty(this.attributes) ? '' : (' ' + this.attributesString() + ' ');
+    var part;
+  
+    if (this.value) {
+      part = '>' + this.value + '</' + this.name + '>';
     }
     else {
-      part = '></' + this.name + '>';
+      if (this.empty) {
+        part = '/>';
+      }
+      else {
+        part = '></' + this.name + '>';
+      }
     }
+  
+    return '<' + this.name + attributes + part;
+  },
+  
+  attributesString: function() {
+    return ArtJs.ArrayUtils.map(ArtJs.ObjectUtils.toArray(this.attributes), ArtJs.ElementBuilder.attributePairToString).join(' ');
   }
-
-  return '<' + this.name + attributes + part;
-};
-
-ArtJs.ElementBuilder.prototype.attributesString = function() {
-  return ArtJs.ArrayUtils.map(ArtJs.ObjectUtils.toArray(this.attributes), this.constructor.attributePairToString).join(' ');
 };
 
 ArtJs.ElementBuilder.attributePairToString = function(arr) {
   return arr[0] + '="' + arr[1] + '"';
+};
+
+ArtJs.$B = ArtJs.ElementBuilder.create = function(name, attributes, value, empty) {
+  return (new ElementBuilder(name, attributes, value, empty)).toString();
 };
