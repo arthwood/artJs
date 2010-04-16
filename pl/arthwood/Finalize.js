0207ecbc7c5e0b12981525b36a3395fb72b4bc5e
artur bilski <arthwood@llp.pl>
2010-04-16 16:35:34 +0200

    ClassUtils - implemented inheritance

--- pl/arthwood/Finalize.js
+++ pl/arthwood/Finalize.js
@@ -1,5 +1,6 @@
 ArtJs.ArrayUtils.init();
 ArtJs.ObjectUtils.init();
+ArtJs.ClassUtils.init();
 ArtJs.StringUtils.init();
 ArtJs.ElementUtils.init();
 ArtJs.DateUtils.init();
---
+++ pl/arthwood/utils/ClassUtils.js
@@ -0,0 +1,52 @@
+ArtJs.ClassUtils = pl.arthwood.utils.ClassUtils = {
+  init: function() {
+    this.eachInstanceMethodDC = ArtJs.$DC(this, this.eachInstanceMethod);
+    this.eachClassMethodDC = ArtJs.$DC(this, this.eachClassMethod);
+    
+    ArtJs.$E = ArtJs.$DC(this, this.extend);
+  },
+  
+  extend: function(base, func, instanceMethods, classMethods) {
+    var builder = function() {
+      var args = ArtJs.$args(arguments);
+      var callee = arguments.callee;
+      var proto = callee.prototype;
+      var func = callee.func;
+      var base = callee.base;
+      
+      func.super = ArtJs.$DC(proto, base);
+      func.apply(proto, args);
+    };
+    
+    builder.prototype = new base();
+    builder.func = func;
+    builder.base = base;
+    
+    this.base = base;
+    this.builder = builder;
+    
+    ArtJs.ObjectUtils.extend(builder.prototype, instanceMethods);
+    ArtJs.ObjectUtils.extend(builder, classMethods);
+    
+    ArtJs.ObjectUtils.eachPair(instanceMethods, this.eachInstanceMethodDC);
+    ArtJs.ObjectUtils.eachPair(classMethods, this.eachClassMethodDC);
+    
+    return builder;
+  },
+  
+  eachInstanceMethod: function(k, v) {
+    var baseProto = this.base.prototype;
+    var builderProto = this.builder.prototype;
+    var origin = baseProto[k];
+    
+    origin && (v.super = ArtJs.$DC(builderProto, origin));
+  },
+  
+  eachClassMethod: function(k, v) {
+    var base = this.base;
+    var builder = this.builder;
+    var origin = base[k];
+    
+    origin && (v.super = ArtJs.$DC(builder, origin));
+  }
+};
