6fd7aed6e82ffe546858d688d986b3cd3782c3ab
artur bilski <arthwood@llp.pl>
2010-04-23 17:03:27 +0200

    renaming in DelegateCollection

--- pl/arthwood/events/DelegateCollection.js
+++ pl/arthwood/events/DelegateCollection.js
@@ -1,38 +1,24 @@
 ArtJs.DelegateCollection = pl.arthwood.events.DelegateCollection = function(delegates) {
   this.delegates = delegates || new Array();
-  this.delegateToResultCallback = ArtJs.$DC(this, this.delegateToResult, false);
+  this.delegateToResultDC = ArtJs.$DC(this, this.delegateToResult, false);
 };
 
 ArtJs.DelegateCollection.prototype = {
   invoke: function() {
-    this.delegateToResultCallback.delegate.args = ArtJs.$A(arguments);
+    this.delegateToResultDC.delegate.args = ArtJs.$A(arguments);
     
-    return ArtJs.ArrayUtils.map(this.delegates, this.delegateToResultCallback);
+    return ArtJs.ArrayUtils.map(this.delegates, this.delegateToResultDC);
   },
 
-  combine: function(delegate) {
+  add: function(delegate) {
     this.delegates.push(delegate);
   },
   
-  remove: function(object, method) {
-    var vDelegate;
-    var n = this.length();
-    var vRemoveAt = ArtJs.ArrayUtils.removeAt;
-    
-    while (n-- > 0) {
-      vDelegate = this.delegates[n];
-      
-      if ((vDelegate.object === object) && (!method || (vDelegate.method === method))) {
-        vRemoveAt(this.delegates, n);
-      }
-    }
-  },
-  
-  removeDelegateAt: function(i) {
+  removeAt: function(i) {
     ArtJs.ArrayUtils.removeAt(this.delegates, i);
   },
   
-  removeDelegate: function(delegate) {
+  remove: function(delegate) {
     ArtJs.ArrayUtils.removeItem(this.delegates, delegate);
   },
   
