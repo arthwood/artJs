0172d8202d85c75bc300aa5fb9caadf0f9c9dca9
artur bilski <arthwood@llp.pl>
2010-05-21 17:08:57 +0200

    Queue prototype syntax

--- com/arthwood/data/Queue.js
+++ com/arthwood/data/Queue.js
@@ -1,34 +1,35 @@
-ArtJs.Queue = com.arthwood.data.Queue = function(queue) {
-  this.onModelChange = new ArtJs.CustomEvent('onModelChange');
-  this._onModelChangeDelegate = ArtJs.$D(this, this._onModelChange);
-  this._list = new ArtJs.List(queue);
-  this._list.onChange.add(this._onModelChangeDelegate);
+ArtJs.Queue = com.arthwood.data.Queue = function(data) {
+  this.onChange = new ArtJs.CustomEvent('Queue::onChange');
+  this._list = new ArtJs.List(data);
+  this._list.onChange.add(ArtJs.$D(this, this._onChange));
 };
 
-ArtJs.Queue.prototype._onModelChange = function() {
-  this.onModelChange.fire(this);
-};
+ArtJs.Queue.prototype = {
+  _onChange: function() {
+    this.onChange.fire(this);
+  },
 
-ArtJs.Queue.prototype.addItem = function(item) {
-  this._list.addItem(item);
-};
-
-ArtJs.Queue.prototype.getItem = function() {
-  var item = this._list.getItemAt(0);
-
-  this._list.removeItemAt(0);
-
-  return item;
-};
-
-ArtJs.Queue.prototype.setQueue = function(queue) {
-  this._list = new ArtJs.List(queue);
-};
-
-ArtJs.Queue.prototype.getLength = function() {
-  return this._list.getLength();
-};
-
-ArtJs.Queue.prototype.isEmpty = function() {
-  return this._list.isEmpty();
+  addItem: function(item) {
+    this._list.addItem(item);
+  },
+  
+  getItem: function() {
+    var item = this._list.getItemAt(0);
+    
+    this._list.removeItemAt(0);
+    
+    return item;
+  },
+  
+  setData: function(data) {
+    this._list = new ArtJs.List(data);
+  },
+  
+  getLength: function() {
+    return this._list.getLength();
+  },
+  
+  empty: function() {
+    return this._list.empty();
+  }
 };
