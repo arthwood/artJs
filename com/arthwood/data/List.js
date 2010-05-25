d51d5789388e2786c839fb750163c2a2094358ff
artur bilski <arthwood@llp.pl>
2010-05-25 12:54:09 +0200

    List update

--- com/arthwood/data/List.js
+++ com/arthwood/data/List.js
@@ -1,6 +1,6 @@
 ArtJs.List = com.arthwood.data.List = function(items) {
-  this._items = items || new Array();
-  this._i = 0;
+  this.items = items || new Array();
+  this.i = 0;
   this.onChange = new ArtJs.CustomEvent('List::onChange');
   this.allowDuplicates = true;
   this.loop = false;
@@ -13,18 +13,18 @@ ArtJs.List.prototype = {
   
   addItemAt: function(item, position, noEvent) {
     if (this.allowDuplicates || !this.hasItem(item)) {
-      this._items = ArtJs.ArrayUtils.insertAt(this._items, position, item);
+      this.items = ArtJs.ArrayUtils.insertAt(this.items, position, item);
   
       if (!noEvent) {
         this.onChange.fire(this);
       }
     }
-  
-    return this._items.length;
+    
+    return this.items.length;
   },
   
   removeItem: function(item, onlyFirst, noEvent) {
-    ArtJs.ArrayUtils.removeItem(this._items, item, onlyFirst);
+    ArtJs.ArrayUtils.removeItem(this.items, item, onlyFirst);
   
     if (!noEvent) {
       this.onChange.fire(this);
@@ -32,7 +32,7 @@ ArtJs.List.prototype = {
   },
   
   removeItemAt: function(position, noEvent) {
-    ArtJs.ArrayUtils.removeAt(this._items, position);
+    ArtJs.ArrayUtils.removeAt(this.items, position);
   
     if (!noEvent) {
       this.onChange.fire(this);
@@ -40,18 +40,18 @@ ArtJs.List.prototype = {
   },
   
   removeAll: function() {
-    this._items.splice(0);
+    this.items.splice(0);
     this.onChange.fire(this);
   },
   
   getItemAt: function(position) {
     position = this.loop ? (ArtJs.MathUtils.periodicLimit(position, 0, this.getLength())) : position;
   
-    return this._items[position];
+    return this.items[position];
   },
   
   getItemIndex: function(item) {
-    return this._items.indexOf(item);
+    return this.items.indexOf(item);
   },
   
   moveItem: function(item, idx) {
@@ -60,21 +60,21 @@ ArtJs.List.prototype = {
   },
   
   getLength: function() {
-    return this._items.length;
+    return this.items.length;
   },
   
   getItems: function() {
-    return this._items.concat();
+    return this.items.concat();
   },
   
   setItems: function(items) {
-    this._items = items;
+    this.items = items;
   
     this.onChange.fire(this);
   },
   
   hasItem: function(item) {
-    return ArtJs.ArrayUtils.include(this._items, item);
+    return ArtJs.ArrayUtils.include(this.items, item);
   },
   
   setItem: function(item) {
@@ -87,50 +87,50 @@ ArtJs.List.prototype = {
   },
   
   reset: function() {
-    this._i = 0;
+    this.i = 0;
   },
   
   getPointer: function() {
-    return this._i;
+    return this.i;
   },
   
   setPointer: function(i) {
-    this._i = i;
+    this.i = i;
   },
   
   decrease: function() {
-    this._i--;
+    this.i--;
   },
   
   increase: function() {
-    this._i++;
+    this.i++;
   },
   
   getCurrent: function() {
-    return this.getItemAt(this._i);
+    return this.getItemAt(this.i);
   },
   
   getPrevious: function() {
-    return this.getItemAt(this._i - 1);
+    return this.getItemAt(this.i - 1);
   },
   
   getNext: function() {
-    return this.getItemAt(this._i + 1);
+    return this.getItemAt(this.i + 1);
   },
   
   getFirst: function() {
-    return ArtJs.ArrayUtils.first(this._items);
+    return ArtJs.ArrayUtils.first(this.items);
   },
   
   getLast: function() {
-    return ArtJs.ArrayUtils.last(this._items);
+    return ArtJs.ArrayUtils.last(this.items);
   },
   
   empty: function() {
-    return ArtJs.ArrayUtils.empty(this._items);
+    return ArtJs.ArrayUtils.empty(this.items);
   },
   
   isLast: function() {
-    return this._i == (this.getLength() - 1);
+    return this.i == (this.getLength() - 1);
   }
 };
