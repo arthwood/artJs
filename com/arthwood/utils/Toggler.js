1556854f2cb231c3a1b17a449c3b27529c48ab4d
arthwood <admin@arturbilski.pl>
2014-02-20 22:09:32 +0000

    TogglerSpec

--- com/arthwood/utils/Toggler.js
+++ com/arthwood/utils/Toggler.js
@@ -1,15 +1,16 @@
-ArtJs.Toggler = com.arthwood.utils.Toggler = function() {
-  this.current = null;
-  this.onActivate = new ArtJs.CustomEvent('Toggler::onActivate');
-  this.onDeactivate = new ArtJs.CustomEvent('Toggler::onDeactivate');
-};
-
-ArtJs.Toggler.prototype = {
-  toggle: function(item) {
-    this.onDeactivate.fire(this);
-    
-    this.current = item;
-    
-    this.onActivate.fire(this);
+ArtJs.Toggler = com.arthwood.utils.Toggler = ArtJs.Class(
+  function() {
+    this.current = null;
+    this.onActivate = new ArtJs.CustomEvent('Toggler::onActivate');
+    this.onDeactivate = new ArtJs.CustomEvent('Toggler::onDeactivate');
+  }, 
+  {
+    toggle: function(item) {
+      this.onDeactivate.fire(this);
+      
+      this.current = item;
+      
+      this.onActivate.fire(this);
+    }
   }
-};
+);
